822b98bd810921ed6d931c0f6aa90e4390a2db32
firakti <faaydemir@outlook.com>
2021-02-27 02:39:46 +0300

    fix on description

--- src/view/auth/parts/landing.js
+++ src/view/auth/parts/landing.js
@@ -12,8 +12,7 @@ const Landing = () => {
         <div className="app-info">
             <h1 className="title">{landingContent.appName}</h1>
             <h2 className="sub-title">{landingContent.subtitle}</h2>
-            <span className="description emphasis">{landingContent.appName}</span>
-            <span className="description">{landingContent.description}</span>
+            <span className="description"><span className="description emphasis">{landingContent.appName}</span>{landingContent.description}</span>
             <img src="image.png" alt="app of ss"></img>
         </div>
     )
